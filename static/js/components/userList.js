b8366bf6f3a3c0a7db306e73f6579521f200d4e8
oleg_livitskyi <olivitskyi@gmail.com>
2024-08-16 00:18:57 +0300

    rr

--- static/js/components/userList.js
+++ static/js/components/userList.js
@@ -1,34 +1,31 @@
-export const renderUserList = (
-  userContainerId,
-  users,
-  currentUserId,
-  onUserClick
-) => {
-  const userContainer = document.getElementById(userContainerId);
-  if (!userContainer || !users.length) return;
+export const renderUserList = (userContainerId, users, currentUserId, onUserClick) => {
+    const userContainer = document.getElementById(userContainerId);
+    if (!userContainer || !users.length) return;
 
-  userContainer.innerHTML = "";
+    // Очищуємо контейнер перед додаванням нових користувачів
+    userContainer.innerHTML = "";
 
-  users.sort((a, b) => {
-    const aLastMessageTime = new Date(a.last_message_time || 0);
-    const bLastMessageTime = new Date(b.last_message_time || 0);
+    // Сортування і рендеринг списку користувачів
+    users.sort((a, b) => {
+        const aLastMessageTime = new Date(a.last_message_time || 0);
+        const bLastMessageTime = new Date(b.last_message_time || 0);
 
-    if (aLastMessageTime > bLastMessageTime) return -1;
-    if (aLastMessageTime < bLastMessageTime) return 1;
+        if (aLastMessageTime > bLastMessageTime) return -1;
+        if (aLastMessageTime < bLastMessageTime) return 1;
 
-    return a.username.localeCompare(b.username);
-  });
-
-  users
-    .filter((user) => user.user_id !== currentUserId)
-    .forEach((user) => {
-      const userElement = document.createElement("div");
-      userElement.classList.add("user-box");
-      userElement.dataset.userId = user.user_id;
-
-      const statusClass = user.is_online ? "logged-in" : "logged-out";
-      userElement.innerHTML = `<span class="${statusClass}">●</span>${user.username}`;
-      userElement.addEventListener("click", () => onUserClick(user.user_id));
-      userContainer.appendChild(userElement);
+        return a.username.localeCompare(b.username);
     });
+
+    users
+        .filter(user => user.user_id !== currentUserId)
+        .forEach(user => {
+            const userElement = document.createElement("div");
+            userElement.classList.add("user-box");
+            userElement.dataset.userId = user.user_id;
+
+            const statusClass = user.is_online ? "logged-in" : "logged-out";
+            userElement.innerHTML = `<span class="${statusClass}">●</span>${user.username}`;
+            userElement.addEventListener("click", () => onUserClick(user.user_id));
+            userContainer.appendChild(userElement);
+        });
 };
