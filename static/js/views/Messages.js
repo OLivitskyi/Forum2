8fda74361d4a43e264a359d9a74f5ef5c3d83fcd
oleg_livitskyi <olivitskyi@gmail.com>
2024-08-17 18:05:25 +0300

    feat: Display logged-in user's name in the welcome message on the Messages page
    - Updated Messages.js to dynamically replace 'User' with the logged-in user's name in the welcome header.
    - Retrieved user information using getUserInfo() and updated the DOM element accordingly.
    ї

--- static/js/views/Messages.js
+++ static/js/views/Messages.js
@@ -11,58 +11,65 @@ import { getUserInfo } from "../api.js";
 import { renderUserList } from "../components/userList.js";
 
 export default class extends AbstractView {
-  constructor(params) {
-    super(params);
-    this.setTitle("Messages");
-  }
-
-  async getHtml() {
-    const content = `
-            <h1>Welcome to your Messages, User!</h1>
-            <div class="message-container">
-                <div class="box" id="box1">
-                    <!-- User list will be dynamically populated here -->
-                </div>
-                <div class="box" id="box2">
-                    <div class="message-list">
-                        <!-- Messages will be dynamically populated here -->
-                    </div>
-                    <div class="chatbox">
-                        <input type="text" id="message-input" placeholder="Write your message here">
-                        <button id="send-button">SEND</button>
-                    </div>
-                </div>
+    constructor(params) {
+      super(params);
+      this.setTitle("Messages");
+    }
+  
+    async getHtml() {
+      const content = `
+        <h1 id="welcome-message">Welcome to your Messages, User!</h1>
+        <div class="message-container">
+          <div class="box" id="box1">
+            <!-- User list will be dynamically populated here -->
+          </div>
+          <div class="box" id="box2">
+            <div class="message-list">
+              <!-- Messages will be dynamically populated here -->
             </div>
-        `;
-
-    return getLayoutHtml(content);
-  }
-
-  async postRender() {
-    try {
-      const currentUserInfo = await getUserInfo();
-      const currentUserId = currentUserInfo.user_id;
-
-      requestUserStatus();
-      renderUserList(
-        "box1",
-        JSON.parse(localStorage.getItem("users")) || [],
-        currentUserId,
-        (receiverID) => {
-          if (receiverID !== currentUserId) {
-            if (document.querySelector(".message-list")) {
-              setCurrentReceiver(receiverID);
-            } else {
-              console.error("Message list not found on the page.");
+            <div class="chatbox">
+              <input type="text" id="message-input" placeholder="Write your message here">
+              <button id="send-button">SEND</button>
+            </div>
+          </div>
+        </div>
+      `;
+  
+      return getLayoutHtml(content);
+    }
+  
+    async postRender() {
+      try {
+        const currentUserInfo = await getUserInfo();
+        const currentUserId = currentUserInfo.user_id;
+        const userName = currentUserInfo.username; 
+  
+        const welcomeMessage = document.getElementById('welcome-message');
+        if (welcomeMessage) {
+          welcomeMessage.textContent = `Welcome to your Messages, ${userName}!`;
+        }
+  
+        requestUserStatus();
+        renderUserList(
+          "box1",
+          JSON.parse(localStorage.getItem("users")) || [],
+          currentUserId,
+          (receiverID) => {
+            if (receiverID !== currentUserId) {
+              if (document.querySelector(".message-list")) {
+                setCurrentReceiver(receiverID);
+              } else {
+                console.error("Message list not found on the page.");
+              }
             }
           }
-        }
-      );
-
-      setupMessageForm();
-      setupMessageListScroll();
-    } catch (error) {
-      console.error("An error occurred during postRender:", error);
+        );
+  
+        setupMessageForm();
+        setupMessageListScroll();
+      } catch (error) {
+        console.error("An error occurred during postRender:", error);
+      }
     }
   }
-}
+  
